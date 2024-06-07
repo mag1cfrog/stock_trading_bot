b01325708fcd2ddf0cca19b3343c4b66c6d1dc13
mag1cfrog <harrywong2017@gmail.com>
2024-06-06 21:53:21 -0700

    Testing on the writing of parquet files through official alpaca-py SDK

--- src/data_fetching/alpaca_py_fetch.py
+++ src/data_fetching/alpaca_py_fetch.py
@@ -2,6 +2,7 @@ import os
 import time
 import logging
 import itertools
+from concurrent.futures import ThreadPoolExecutor
 import json
 import threading
 from datetime import datetime, timedelta
@@ -60,44 +61,89 @@ from alpaca.data.timeframe import TimeFrame
 #     logging.info(f"Total API calls made: {calls}")
 
 
-def call_api_for_duration(client, request_params, duration):
-    end_time = time.time() + duration
+# def call_api_for_duration(client, request_params, duration):
+#     end_time = time.time() + duration
+#     calls = 0
+
+#     while time.time() < end_time:
+#         try:
+#             response = client.get_stock_bars(request_params)
+#             calls += 1
+#             file_path = f'./data/landing/response_{calls}.json'
+#             data = itertools.chain.from_iterable(response.dict().values())
+#             # with open(file_path, 'w') as f:
+#             #     json.dump(list(data), f, default=str)
+#             logging.info(f"API call {calls} successful, data saved to {file_path}")
+
+#         except Exception as e:
+#             logging.error(f"Error on request {calls}: {str(e)}")
+#             break
+
+#     logging.info(f"Total API calls made: {calls}")
+
+
+# def test_alpaca_api_with_longest_time_range(symbol="NVDA", timeframe=TimeFrame.Day, duration=60, time_back=1):
+
+    
+#     api_key = os.getenv('APCA_API_KEY_ID')
+#     secret_key = os.getenv('APCA_API_SECRET_KEY')
+
+#     client = StockHistoricalDataClient(api_key, secret_key)
+
+#     os.makedirs('./data/landing', exist_ok=True)
+    
+
+#     # Define initial time range
+#     end_date = datetime.now() - timedelta(days=1)
+#     # This alpaca api can get data since 2016
+#     start_date = datetime(2016, 1, 1)
+    
+
+#     request_params = StockBarsRequest(
+#         symbol_or_symbols=[symbol],
+#         timeframe=timeframe,
+#         start=start_date,
+#         end=end_date
+#     )
+
+#     # Create a separate thread for calling the API
+#     api_thread = threading.Thread(target=call_api_for_duration, args=(client, request_params, duration))
+#     api_thread.start()
+#     api_thread.join()  # Wait for the thread to finish if necessary
+
+
+def call_api(client, request_params, stop_event):
     calls = 0
 
-    while time.time() < end_time:
+    while not stop_event.is_set():
         try:
             response = client.get_stock_bars(request_params)
             calls += 1
-            file_path = f'./data/landing/response_{calls}.json'
-            data = itertools.chain.from_iterable(response.dict().values())
-            # with open(file_path, 'w') as f:
-            #     json.dump(list(data), f, default=str)
-            logging.info(f"API call {calls} successful, data saved to {file_path}")
+            json_file_path = f'./data/landing/response_{calls}.json'
+            parquet_file_path = f'./data/landing/response_{calls}.parquet'
+            data_df = response.df.reset_index(drop=False)
+            # Save data to parquet file
+            data_df.to_parquet(parquet_file_path)
+            
+            # Save data to json file
+            # data_dict = data_df.to_dict(orient='records')
+            # with open(json_file_path, 'w') as f:
+            #     json.dump(list(data_dict), f, default=str)
 
+            logging.info(f"API call {calls} successful, data saved to {json_file_path}")
         except Exception as e:
             logging.error(f"Error on request {calls}: {str(e)}")
             break
 
     logging.info(f"Total API calls made: {calls}")
 
-
-def test_alpaca_api_with_longest_time_range(symbol="NVDA", timeframe=TimeFrame.Day, duration=60, time_back=1):
-
-    
+def test_alpaca_api_with_longest_time_range(symbol="NVDA", timeframe=TimeFrame.Day, duration=60):
     api_key = os.getenv('APCA_API_KEY_ID')
     secret_key = os.getenv('APCA_API_SECRET_KEY')
-
     client = StockHistoricalDataClient(api_key, secret_key)
-
     os.makedirs('./data/landing', exist_ok=True)
-    
-
-    # Define initial time range
     end_date = datetime.now() - timedelta(days=1)
-    # This alpaca api can get data since 2016
     start_date = datetime(2016, 1, 1)
-    
-
     request_params = StockBarsRequest(
         symbol_or_symbols=[symbol],
         timeframe=timeframe,
@@ -105,10 +151,15 @@ def test_alpaca_api_with_longest_time_range(symbol="NVDA", timeframe=TimeFrame.D
         end=end_date
     )
 
-    # Create a separate thread for calling the API
-    api_thread = threading.Thread(target=call_api_for_duration, args=(client, request_params, duration))
+    # Create a thread for calling the API
+    stop_event = threading.Event()
+    api_thread = threading.Thread(target=call_api, args=(client, request_params, stop_event))
     api_thread.start()
-    api_thread.join()  # Wait for the thread to finish if necessary
+    
+    # Allow the thread to run for the specified duration
+    time.sleep(duration)
+    stop_event.set()  # Signal the thread to stop making API calls
+    api_thread.join()  # Wait for the thread to finish
 
 
 def main():

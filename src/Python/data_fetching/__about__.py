58e65f89d560648a1bb4d2ea786bc86a1e351ecd
mag1cfrog <harrywong2017@gmail.com>
2024-08-07 17:39:38 -0700

    refactor the folder structure

---
+++ src/Python/data_fetching/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2024-present mag1cfrog <harrywong2017@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.0.1"
---
+++ tests/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2024-present mag1cfrog <harrywong2017@gmail.com>
+#
+# SPDX-License-Identifier: MIT

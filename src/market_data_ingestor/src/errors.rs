50e1b05c5720c72f94108f8ee9a3d4489d72559f
mag1cfrog <harrywong2017@gmail.com>
2025-07-08 23:18:18 -0700

    refactor: Remove unused error variants from Error enum

--- src/market_data_ingestor/src/errors.rs
+++ src/market_data_ingestor/src/errors.rs
@@ -16,12 +16,4 @@ pub enum Error {
     /// An error related to configuration.
     #[error("Configuration error: {0}")]
     Config(String),
-
-    /// A generic I/O error.
-    #[error("I/O error: {0}")]
-    Io(#[from] std::io::Error),
-
-    /// An error from the Polars library.
-    #[error("Polars operation failed: {0}")]
-    Polars(#[from] polars::prelude::PolarsError),
 }

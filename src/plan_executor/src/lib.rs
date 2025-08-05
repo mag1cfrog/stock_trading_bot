1999c53bddaa02be232035bb9377d60b49c03ecd
mag1cfrog <harrywong2017@gmail.com>
2025-08-04 21:02:29 -0700

    feat: add asset_refresh_planner module and remove plan_executor

--- src/plan_executor/src/lib.rs
+++
@@ -1,6 +0,0 @@
-//! This crate handles the execution of trading plans.
-//! 
-//! It provides functionality to process and execute trading strategies
-//! based on predefined plans.
-
-#![deny(missing_docs)]

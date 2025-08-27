4ca9b97a96c313b7bddd55cc82e5047aba145bed
mag1cfrog <harrywong2017@gmail.com>
2025-08-26 19:26:41 -0700

    feat: remove additional asset-related tables in down migration script

--- src/asset_sync/migrations/2025-08-24-040310_schema_v1/down.sql
+++ src/asset_sync/migrations/2025-08-24-040310_schema_v1/down.sql
@@ -1,3 +1,7 @@
 DROP TABLE IF EXISTS asset_gaps;
 DROP TABLE IF EXISTS asset_coverage_bitmap;
 DROP TABLE IF EXISTS asset_manifest;
+DROP TABLE IF EXISTS provider_symbol_map;
+DROP TABLE IF EXISTS provider_asset_class;
+DROP TABLE IF EXISTS asset_class;
+DROP TABLE IF EXISTS provider;
